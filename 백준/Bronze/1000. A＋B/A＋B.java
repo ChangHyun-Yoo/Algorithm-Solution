f993f7f229e5052f375a30f6d4b0fa883d7b2358
ChangHyun Yoo <43163892+ChangHyun-Yoo@users.noreply.github.com>
2023-01-13 22:10:00 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() + scanner.nextInt());
+    }
+
+}
