6ed64524ebb90cdcb7871b6d3760bc329bbc17f3
ChangHyun Yoo <43163892+ChangHyun-Yoo@users.noreply.github.com>
2023-02-15 23:56:39 +0900

    [Gold III] Title: 박스 채우기, Time: 808 ms, Memory: 58372 KB -BaekjoonHub

---
+++ 백준/Gold/1493. 박스 채우기/박스 채우기.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int length, width, height;	// 박스 크기
+	static int n;						// 큐브 종류 개수 (최대 20)
+	static int[] cubes;					// 각 정육면체 큐브의 한 변의 길이 크기
+	static int minCount;				// 출력: 큐브 최소 개수
+	static boolean flag;				// 출력: 채울 수 없는 경우 판단
+
+	/* l, w, h: 채울 영역 크기 length, width, height */
+	static void solution(int l, int w, int h) {
+		// 채울 영역이 없는 경우 (이전 단계에서 큐브로 채웠을 때, 영역이 맞아 떨어지는 경우)
+		if (l == 0 || w == 0 || h == 0)
+			return;
+
+		int minSize = l;		// minSize: l, w, h 중에 min
+		if (minSize > w)
+			minSize = w;
+		if (minSize > h)
+			minSize = h;
+
+		flag = false;
+		int cubeIdx = baseLog(2, minSize);
+		int cubeLen = 0;
+
+		// 채울 수 있는 가장 큰 큐브 찾음
+		for (int i = cubeIdx; i >= 0; i--) {
+			if (cubes[i] > 0) {				// 큐브 개수 > 0 (큐브가 있는 경우)
+				minCount++;					// 채울 수 있는 가장 큰 큐브로 채움
+				cubes[i]--;
+				cubeLen = 1 << i;			// 큐브의 한 변 길이: 2^i
+
+				flag = true;
+				break;
+			}
+		}
+
+		if (!flag)			// 채울 수 없는 경우 => 큐브 개수 부족한 경우
+			return;
+
+		// 채우고 남은 영역에 대해 재귀 호출하여, 분할 정복
+		solution(cubeLen, w - cubeLen, cubeLen);
+		solution(l - cubeLen, w, cubeLen);
+		solution(l, w, h - cubeLen);
+	}
+
+	static int baseLog(int base, int x) {
+		return (int)(Math.log(x) / Math.log(base));
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in)
+		);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		length = Integer.parseInt(st.nextToken());
+		width = Integer.parseInt(st.nextToken());
+		height = Integer.parseInt(st.nextToken());
+
+		n = Integer.parseInt(br.readLine());		// 큐브 종류 개수
+		cubes = new int[21];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int idx = Integer.parseInt(st.nextToken());
+			int count = Integer.parseInt(st.nextToken());
+			cubes[idx] = count;		// 길이 2^idx 인 큐브가 count 개수
+		}
+
+		solution(length, width, height);
+
+		if (flag)
+			System.out.println(minCount);
+		else
+			System.out.println(-1);
+	}
+}
