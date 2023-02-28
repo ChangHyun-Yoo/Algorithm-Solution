62bc3f81818dc0783ecac2d0dd24f376b7ccca47
ChangHyun Yoo <43163892+ChangHyun-Yoo@users.noreply.github.com>
2023-02-28 22:37:03 +0900

    [Silver II] Title: 민균이의 계략, Time: 140 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] info;
+    static int[] result;
+    static int length;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        info = new int[N];
+        result = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++) {
+            info[i] = Integer.parseInt(st.nextToken());
+        }
+
+        result[0] = info[0];
+        length = 1;
+        int current = 1;
+        while(current != N) {
+            int index = binarySearch(info[current]);
+            if(index == -1) {
+                result[length++] = info[current++];
+            } else {
+                result[index] = info[current++];
+            }
+        }
+
+        System.out.println(length);
+    }
+
+    static int binarySearch(int value) {
+        int low = 0;
+        int high = length;
+
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            if(value <= result[mid]) {
+                high = mid;
+            } else if(value > result[mid]) {
+                low = mid + 1;
+            }
+        }
+
+        if(low == length) {
+            return -1;
+        } else {
+            return low;
+        }
+    }
+}
