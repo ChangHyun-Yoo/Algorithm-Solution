6c619b21b586c16e092034e4c96e7272d5cd93d8
ChangHyun Yoo <43163892+ChangHyun-Yoo@users.noreply.github.com>
2023-04-15 00:44:04 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT board_id, writer_id, title, price, if(status = 'SALE', '판매중', if(status = 'RESERVED', '예약중', '거래완료')) from used_goods_board where created_date = '2022-10-05' order by board_id desc
