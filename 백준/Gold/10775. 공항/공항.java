85b4543f8c09acca7cb3d1d7396c5a9e07206f23
ChangHyun Yoo <43163892+ChangHyun-Yoo@users.noreply.github.com>
2023-05-26 17:50:00 +0900

    [Gold II] Title: 공항, Time: 252 ms, Memory: 22428 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.java
+++ 백준/Gold/10775. 공항/공항.java
@@ -1,64 +1,45 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] parent;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int G = Integer.parseInt(br.readLine());
-		int P = Integer.parseInt(br.readLine());
+    static int[] parent;
 
-		parent = new int[G + 1];
-		for (int i = 1; i <= G; i++) {
-			parent[i] = i;
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		// g번 비행기는 g번 이하 게이트에만 도킹이 가능함.
-		// 이 때, g번 비행기를 g번 게이트에 도킹하는 것이 최선임.
-		// 만약, g번 비행기를 g번게이트에 도킹할 수 없다면,
-		// g-1번 게이트에 차선책으로 도킹시킴.
-		// g-1번도 안 된다면, g-2번, ... 0번까지 탐색
-		// 이 때, 차선책이 0번을 가리키고 있으면 도킹이 불가능한 상태임.
-		// 차선책을 찾는 과정에서 유니온 파인드를 사용함.
-		int ans = 0;
-		for (int i = 0; i < P; i++) {
-			int g = Integer.parseInt(br.readLine());
-			int emptyGate = find(g);
+        int G = Integer.parseInt(br.readLine());
+        int P = Integer.parseInt(br.readLine());
 
-			if (emptyGate == 0) {
-				break;
-			}
+        parent = new int[G + 1];
+        for(int i = 0; i < G + 1; i++) {
+            parent[i] = i;
+        }
 
-			ans++;
-			union(emptyGate, emptyGate - 1);
-		}
+        int answer = 0;
+        for(int i = 0; i < P; i++) {
+            int p = Integer.parseInt(br.readLine());
+            int find = findRoot(p);
+            if(find == 0) break;
+            else {
+                union(find - 1, find);
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
 
-		bw.write(ans + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+    static int findRoot(int i) {
+        if(i == parent[i]) return i;
 
-	public static int find(int x) {
-		if (x == parent[x]) {
-			return x;
-		}
+        return parent[i] = findRoot(parent[i]);
+    }
 
-		return parent[x] = find(parent[x]);
-	}
+    static void union(int i, int j) {
+        int rootI = findRoot(i);
+        int rootJ = findRoot(j);
 
-	public static void union(int x, int y) {
-		x = find(x);
-		y = find(y);
-
-		if (x != y) {
-			parent[x] = y;
-		}
-	}
-
+        if(rootI != rootJ)
+            parent[rootJ] = rootI;
+    }
 }
