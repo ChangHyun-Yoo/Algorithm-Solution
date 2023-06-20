6fea4c9cd3fb72027306b53d9d8c4043f2691f9e
ChangHyun Yoo <43163892+ChangHyun-Yoo@users.noreply.github.com>
2023-06-20 15:29:39 +0900

    [Bronze I] Title: 소인수분해, Time: 172 ms, Memory: 14876 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while(N != 1) {
+            for(int i = 2; i <= N; i++) {
+                if(N % i == 0) {
+                    sb.append(i).append('\n');
+                    N /= i;
+                    break;
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
