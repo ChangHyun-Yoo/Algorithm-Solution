f349c9a1b918287b9cd43bdcd2325e3c079c6d12
ChangHyun Yoo <43163892+ChangHyun-Yoo@users.noreply.github.com>
2023-07-02 22:39:00 +0900

    [Gold III] Title: 나머지 합, Time: 528 ms, Memory: 116548 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -15,25 +15,20 @@ public class Main {
         st = new StringTokenizer(br.readLine(), " ");
         long sum = 0;
 
-        Map<Integer, Integer> m = new HashMap<>();
-
         for(int i = 0; i < N; i++) {
             sum += Integer.parseInt(st.nextToken());
             lst[i + 1] = (int) (sum % M);
         }
 
-        for(int r: lst) {
-            if(m.get(r) == null) {
-                m.put(r, 1);
-            } else {
-                m.replace(r, m.get(r) + 1);
-            }
+        int[] r = new int[M];
+        for(int i: lst) {
+            r[i]++;
         }
 
         long answer = 0;
         for(int i = 0; i < lst.length - 1; i++) {
-            m.replace(lst[i], m.get(lst[i]) - 1);
-            answer += m.get(lst[i]);
+            r[lst[i]]--;
+            answer += r[lst[i]];
         }
         System.out.println(answer);
     }
