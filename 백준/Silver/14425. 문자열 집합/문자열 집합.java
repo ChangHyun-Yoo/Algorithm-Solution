3172f215bc8cff0c289578dd045903adce777053
ChangHyun Yoo <jakey1110@naver.com>
2024-03-04 17:26:41 +0900

    [Silver IV] Title: 문자열 집합, Time: 5888 ms, Memory: 1152900 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,13 +1,7 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static class TrieNode {
-        Map<Character, TrieNode> child = new HashMap<>();
-        boolean isLast = false;
-        public TrieNode() {};
-    }
 
     static class Trie {
         TrieNode root;
@@ -18,42 +12,50 @@ public class Main {
 
         public void insert(String s) {
             TrieNode node = root;
-            for(int i = 0; i < s.length(); i++) {
+            for (int i = 0; i < s.length(); i++) {
                 node = node.child.computeIfAbsent(s.charAt(i), key -> new TrieNode());
             }
             node.isLast = true;
         }
 
-        public boolean exists(String s) {
+        public boolean isExists(String s) {
             TrieNode node = root;
-            for(int i = 0; i < s.length(); i++) {
+            for (int i = 0; i < s.length(); i++) {
                 node = node.child.getOrDefault(s.charAt(i), null);
                 if(node == null) return false;
             }
-            if(node != null)
-                if(node.isLast)
-                    return true;
+            if(node == null) return false;
+            else {
+                if(node.isLast) return true;
                 else return false;
-            else return false;
+            }
         }
+    }
+
+    static class TrieNode {
+        Map<Character, TrieNode> child = new HashMap<>();
+        boolean isLast = false;
+        public TrieNode() {};
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         Trie trie = new Trie();
-        for(int i = 0; i < N; i++) {
+
+        for (int i = 0; i < N; i++) {
             trie.insert(br.readLine());
         }
 
         int answer = 0;
-        for(int i = 0; i < M; i++) {
-            String b = br.readLine();
-            if(trie.exists(b)) answer++;
+        for (int i = 0; i < M; i++) {
+            if(trie.isExists(br.readLine())) answer++;
         }
+
         System.out.println(answer);
     }
 }
