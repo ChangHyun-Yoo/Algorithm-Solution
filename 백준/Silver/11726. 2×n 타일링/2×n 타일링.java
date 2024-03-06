6db981f3c402a50bd616ae56b8da7ab6595760de
ChangHyun Yoo <jakey1110@naver.com>
2024-03-06 17:36:01 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if(n == 1 || n == 2) {
+            System.out.println(n);
+            return;
+        }
+
+        dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
