1f0fdf1946ee11155a17d34ecbc06155c3a14ac7
ChangHyun Yoo <jakey1110@naver.com>
2024-03-12 20:26:07 +0900

    [Gold IV] Title: 트리의 지름, Time: 268 ms, Memory: 23252 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,72 +1,89 @@
-import java.io.*;
-import java.lang.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    public static int max = -1;
-    public static int maxNode = -1;
+    static int n;
+    static List<List<Node>> roads = new ArrayList<>();
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        int[] length = new int[n + 1];
-        Map<Integer, List<Integer>> road = new HashMap<>();
-        for(int i = 1; i <= n; i++) {
-            road.put(i, new ArrayList<>());
+        n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n + 1; i++) {
+            roads.add(new ArrayList<>());
         }
+        visited = new boolean[n + 1];
 
-        if(n == 1) {
-            System.out.println(0);
-            return;
-        }
-
-        for (int i = 0; i < n - 1; i++) {
+        for(int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int parent = Integer.parseInt(st.nextToken());
-            int child = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
 
-            road.get(parent).add(child);
-            road.get(parent).add(weight);
-            road.get(child).add(parent);
-            road.get(child).add(weight);
-            length[child] = length[parent] + weight;
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+
+            roads.get(s).add(new Node(e, d));
+            roads.get(e).add(new Node(s, d));
         }
 
-        List<Integer> ll = new ArrayList<>();
-        for(int l: length) {
-            ll.add(l);
+        int max = -1;
+        int maxNode = 0;
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(1, 0));
+
+        while(!q.isEmpty()) {
+            Node now = q.poll();
+
+            visited[now.num] = true;
+            if(max < now.dis) {
+                max = now.dis;
+                maxNode = now.num;
+            }
+
+            for(Node next: roads.get(now.num)) {
+                if(!visited[next.num]) {
+                    q.offer(new Node(next.num, now.dis + next.dis));
+                }
+            }
         }
-        int firstIndex = ll.indexOf(Collections.max(ll));
 
-        boolean[] visited = new boolean[n + 1];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(firstIndex);
-        visited[firstIndex] = true;
+        int first = maxNode;
+        max = -1;
+        maxNode = 0;
+        visited = new boolean[n + 1];
 
-        bfs(visited, road, 0, firstIndex);
+        q.clear();
+        q.offer(new Node(first, 0));
+
+        while(!q.isEmpty()) {
+            Node now = q.poll();
+
+            visited[now.num] = true;
+            if(max < now.dis) {
+                max = now.dis;
+                maxNode = now.num;
+            }
+
+            for(Node next: roads.get(now.num)) {
+                if(!visited[next.num]) {
+                    q.offer(new Node(next.num, now.dis + next.dis));
+                }
+            }
+        }
+
         System.out.println(max);
     }
 
-    public static void bfs(boolean[] visited, Map<Integer, List<Integer>> road, int sum, int current) {
-        boolean b = false;
-        for(int i = 0; i < road.get(current).size() / 2; i++) {
-            if(visited[road.get(current).get(2*i)]) {//방문한 적 있으면
-                continue;
-            } else {//방문한 적 없으면
-                b = true;
-                boolean[] copy = visited.clone();
-                copy[current] = true;
-                bfs(copy, road, sum + road.get(current).get(2*i + 1), road.get(current).get(2*i));
-            }
-        }
-        if(!b) {
-            if(sum > max) {
-                max = sum;
-                maxNode = current;
-            }
+    static class Node {
+        int num;
+        int dis;
+
+        public Node(int num, int dis) {
+            this.num = num;
+            this.dis = dis;
         }
     }
 }
