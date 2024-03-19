b80b818ee8d001640f86e395f6d6107f1534c2c5
ChangHyun Yoo <jakey1110@naver.com>
2024-03-19 21:27:10 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 4.96 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
@@ -0,0 +1,76 @@
+import java.util.*;
+class Solution {
+    
+    public int solution(int n, int[] cores) {
+        if(cores.length >= n) return cores[n - 1];
+        
+        int low = 0;
+        int high = 500000001;
+        
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            
+            int p = 0;
+            for(int core: cores) {
+                p += mid / core;
+                if(mid % core != 0) p++;
+            }
+            if(p >= n) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        low--;
+        
+        int inputed = 0;
+        
+        PriorityQueue<Core> pq = new PriorityQueue<>();
+        for(int i = 0; i < cores.length; i++) {
+            inputed += low / cores[i];
+            if(low % cores[i] != 0) inputed++;
+            pq.offer(new Core(cores[i], i + 1, low % cores[i]));
+        }
+        
+        int answer = 0;
+        while(!pq.isEmpty()) {
+            Core c = pq.poll();
+            
+            inputed++;
+            if(inputed == n) {
+                answer = c.num;
+                break;
+            }
+            c.finish += c.time;
+            pq.offer(c);
+        }
+        
+        return answer;
+    }
+    
+    static class Core implements Comparable<Core> {
+        int time;
+        int num;
+        long finish;
+        
+        public Core(int time, int num, long finish) {
+            this.time = time;
+            this.num = num;
+            this.finish = finish;
+        }
+        
+        public int compareTo(Core c) {
+            if(this.finish == c.finish) {
+                return this.num - c.num;
+            } else {
+                if(this.finish < c.finish) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+                // return this.finish - c.finish;
+            }
+        }
+    }
+}
