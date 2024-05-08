43bafa0c74ee70c993a9a965434ac8c706027667
ChangHyun Yoo <jakey1110@naver.com>
2024-05-08 15:32:22 +0900

    [Gold II] Title: 나누기, Time: 292 ms, Memory: 24336 KB, Score: 37 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.java
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -15,19 +15,17 @@ public class Main {
             sum[i + 1] = sum[i] + Integer.parseInt(st.nextToken());
         }
 
-        int answer = 0;
+        long answer = 0;
 
-        int fir = 0;
-        int sec = 0;
-        int thi = 0;
-        int fou = 0;
         for(int i = 1; i <= N - 3; i++) {
-            fir = sum[i] - sum[0];
-            for(int j = i + 1; j <= N - 2; j++) {
-                if(sum[j] - sum[i] == fir) {
-                    for(int k = j + 1; k <= N - 1; k++) {
-                        if(sum[k] - sum[j] == fir && sum[N] - sum[k] == fir) {
-                            answer++;
+            int fir = sum[i] - sum[0];
+            if(fir == sum[N] / 4) {
+                for(int j = i + 1; j <= N - 2; j++) {
+                    if(sum[j] - sum[i] == fir) {
+                        for(int k = j + 1; k <= N - 1; k++) {
+                            if(sum[k] - sum[j] == fir && sum[N] - sum[k] == fir) {
+                                answer++;
+                            }
                         }
                     }
                 }
