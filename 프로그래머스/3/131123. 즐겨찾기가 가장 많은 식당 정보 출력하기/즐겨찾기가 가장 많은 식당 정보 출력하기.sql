a3e486570cd806b205429fc77b4da471292d4791
ChangHyun Yoo <jakey1110@naver.com>
2024-05-25 17:50:13 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,5 @@
+select r1.food_type, r1.rest_id, r1.rest_name, r1.favorites from rest_info r1 join
+(select food_type, max(favorites) as 'favorites' from rest_info
+group by food_type) r2
+on r1.food_type = r2.food_type and r1.favorites = r2.favorites
+order by r1.food_type desc
