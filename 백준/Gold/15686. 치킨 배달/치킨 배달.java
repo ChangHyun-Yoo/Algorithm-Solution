84f6b0f67d1e892775e3de96923d033c9476b28f
ChangHyun Yoo <jakey1110@naver.com>
2025-08-27 23:14:02 +0900

    [Gold V] Title: 치킨 배달, Time: 304 ms, Memory: 21448 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,78 +1,85 @@
-import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static List<List<Integer>> home = null;
-    public static List<List<Integer>> choice  = new ArrayList<>();
+
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        List<Integer> choices = new ArrayList<>();
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        List<List<Integer>> store = new ArrayList<>();
-        home = new ArrayList<>();
+        List<Node> homes = new ArrayList<>();
+        List<Node> stores = new ArrayList<>();
 
         for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                if(num == 1) {
-                    List<Integer> point = new ArrayList<>();
-                    point.add(i);
-                    point.add(j);
-                    home.add(point);
-                } else if(num == 2) {
-                    List<Integer> point = new ArrayList<>();
-                    point.add(i);
-                    point.add(j);
-                    store.add(point);
+                int value = Integer.parseInt(st.nextToken());
+
+                // home
+                if(value == 1) {
+                    homes.add(new Node(i, j));
+                }
+                // store
+                else if(value == 2) {
+                    stores.add(new Node(i, j));
                 }
             }
         }
-        List<Integer> current = new ArrayList<>();
-        com(0, current, store, M);
 
-        int[][] lengths = new int[home.size()][store.size()];
-        for(int i = 0; i < home.size(); i++) {
-            for(int j = 0; j < store.size(); j++) {
-                lengths[i][j] = Math.abs(store.get(j).get(0) - home.get(i).get(0)) + Math.abs(store.get(j).get(1) - home.get(i).get(1));
-            }
-        }
+        dfs(0, 0, choices, stores, homes, M);
 
-        int min = 100000;
-
-        for(List<Integer> ch: choice) {
-            int sum = 0;
-            for(int i = 0; i < home.size(); i++) {
-                int minn = 100000;
-                for(int j = 0; j < ch.size(); j++) {
-                    if(lengths[i][ch.get(j)] < minn)
-                        minn = lengths[i][ch.get(j)];
-                }
-                sum += minn;
-            }
-            if(sum < min)
-                min = sum;
-        }
-
-        System.out.println(min);
+        System.out.println(answer);
     }
 
-    public static void com(int index, List<Integer> current, List<List<Integer>> store, int M) {
-        if (index == store.size() || current.size() == M) {
-            choice.add(current);
-        } else if (current.size() + store.size() - index == M) {//나머지 모두 선택해야 하는 경우
-            for (int i = index; i < store.size(); i++) {
-                current.add(i);
+    static void dfs(int current, int chosenNum, List<Integer> choices, List<Node> stores, List<Node> homes, int M) {
+        if(current == stores.size() && chosenNum != 0) {
+            int city = 0;
+            for(Node home: homes) {
+                int min = Integer.MAX_VALUE;
+                for(int j = 0; j < stores.size(); j++) {
+                    if(choices.get(j) == 1) {
+                        min = Math.min(min, Math.abs(stores.get(j).x - home.x) + Math.abs(stores.get(j).y - home.y));
+                    }
+                }
+                city += min;
             }
-            com(store.size(), current, store, M);
-        } else {
-            com(index + 1, new ArrayList<>(current), store, M);
-            List<Integer> copy = new ArrayList<>(current);
-            copy.add(index);
-            com(index + 1, copy, store, M);
+
+            answer = Math.min(answer, city);
+            return;
+        } else if(current == stores.size() && chosenNum == 0) return;
+
+        if(chosenNum < M) {
+            choices.add(1);
+            dfs(current + 1, chosenNum + 1, choices, stores, homes, M);
+            choices.remove(choices.size() - 1);
+        }
+        choices.add(0);
+        dfs(current + 1, chosenNum, choices, stores, homes, M);
+        choices.remove(choices.size() - 1);
+
+    }
+
+    static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
+
+
