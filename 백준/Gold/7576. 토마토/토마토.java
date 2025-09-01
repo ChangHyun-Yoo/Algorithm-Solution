77bc0424941e10528286dcfca6119ca802f91e56
ChangHyun Yoo <jakey1110@naver.com>
2025-09-01 22:14:28 +0900

    [Gold V] Title: 토마토, Time: 608 ms, Memory: 125360 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,103 +1,89 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-
         int[][] tomatoes = new int[N][M];
         boolean[][] visited = new boolean[N][M];
-        int all = 0;
-        Queue<Integer> queue = new LinkedList<>();
 
+        Queue<Node> q = new LinkedList<>();
+        for(int n = 0; n < N; n++) {
+            st = new StringTokenizer(br.readLine());
+
+            for(int m = 0; m < M; m++) {
+                int tomato = Integer.parseInt(st.nextToken());
+                tomatoes[n][m] = tomato;
+
+                if(tomato == 1) {
+                    q.offer(new Node(n, m));
+                    visited[n][m] = true;
+                }
+            }
+        }
+
+        int answer = -1;
+        while(!q.isEmpty()) {
+            int size = q.size();
+
+            for(int i = 0; i < size; i++) {
+                Node now = q.poll();
+
+                if(now.x != 0 && !visited[now.x - 1][now.y] && tomatoes[now.x - 1][now.y] != -1) {
+                    tomatoes[now.x - 1][now.y] = 1;
+                    visited[now.x - 1][now.y] = true;
+
+                    q.offer(new Node(now.x - 1, now.y));
+                }
+
+                if(now.y != 0 && !visited[now.x][now.y - 1] && tomatoes[now.x][now.y - 1] != -1) {
+                    tomatoes[now.x][now.y - 1] = 1;
+                    visited[now.x][now.y - 1] = true;
+
+                    q.offer(new Node(now.x, now.y - 1));
+                }
+
+                if(now.x != N - 1 && !visited[now.x + 1][now.y] && tomatoes[now.x + 1][now.y] != -1) {
+                    tomatoes[now.x + 1][now.y] = 1;
+                    visited[now.x + 1][now.y] = true;
+
+                    q.offer(new Node(now.x + 1, now.y));
+                }
+
+                if(now.y != M - 1 && !visited[now.x][now.y + 1] && tomatoes[now.x][now.y + 1] != -1) {
+                    tomatoes[now.x][now.y + 1] = 1;
+                    visited[now.x][now.y + 1] = true;
+
+                    q.offer(new Node(now.x, now.y + 1));
+                }
+            }
+
+            answer++;
+        }
+
+        boolean isZeroExists = false;
         for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
             for(int j = 0; j < M; j++) {
-                int t = Integer.parseInt(st.nextToken());
-                tomatoes[i][j] = t;
-                if(t == 0)
-                    all++;
-                else if(t == 1) {
-                    all++;
-                    queue.add(i);
-                    queue.add(j);
-                    queue.add(0);
-                    visited[i][j] = true;
-                }
+                if(tomatoes[i][j] == 0) isZeroExists = true;
             }
         }
 
-        int currentLevel = -1;
-        while(!queue.isEmpty()) {
-            int row = queue.poll();
-            int col = queue.poll();
-            int level = queue.poll();
-            if(currentLevel != level) {//레벨이 바뀌면
-                currentLevel = level;
-            }
-
-            try {
-                if(tomatoes[row-1][col] == 0) {
-                    tomatoes[row-1][col] = 1;
-                    queue.add(row-1);
-                    queue.add(col);
-                    queue.add(level + 1);
-                }
-            } catch(Exception e) {
-
-            }
-
-            try {
-                if(tomatoes[row+1][col] == 0) {
-                    tomatoes[row+1][col] = 1;
-                    queue.add(row+1);
-                    queue.add(col);
-                    queue.add(level + 1);
-                }
-            } catch(Exception e) {
-
-            }
-
-            try {
-                if(tomatoes[row][col-1] == 0) {
-                    tomatoes[row][col-1] = 1;
-                    queue.add(row);
-                    queue.add(col-1);
-                    queue.add(level + 1);
-                }
-            } catch(Exception e) {
-
-            }
-
-            try {
-                if(tomatoes[row][col+1] == 0) {
-                    tomatoes[row][col+1] = 1;
-                    queue.add(row);
-                    queue.add(col+1);
-                    queue.add(level + 1);
-                }
-            } catch(Exception e) {
-
-            }
-        }
-
-        int hmm = 0;
-        for(int[] tom: tomatoes) {
-            for(int t: tom) {
-                if(t == 1)
-                    hmm++;
-            }
-        }
-
-        if(hmm != all) {
-            System.out.println(-1);
-        } else {
-            System.out.println(currentLevel);
-        }
+        if(isZeroExists) System.out.println(-1);
+        else System.out.println(answer);
     }
 
+    static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
