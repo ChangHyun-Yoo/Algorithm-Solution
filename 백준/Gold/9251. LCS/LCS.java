1090b339146660961ad7d19034a33555a58c238b
ChangHyun Yoo <jakey1110@naver.com>
2025-09-05 14:51:41 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 19860 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,27 +1,38 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static int[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] c1 = br.readLine().toCharArray();
-        char[] c2 = br.readLine().toCharArray();
 
-        dp = new int[c1.length + 1][c2.length + 1];
+        String first = br.readLine();
+        String second = br.readLine();
 
-        for(int i = 1; i < c1.length + 1; i++) {
-            for(int j = 1; j < c2.length + 1; j++) {
-                if(c1[i - 1] == c2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
+        int n = first.length();
+        int m = second.length();
+
+        int[][] dp = new int[n][m];
+
+        if(first.charAt(0) == second.charAt(0)) dp[0][0] = 1;
+        for(int i = 1; i < n; i++) {
+            if(first.charAt(i) == second.charAt(0) || dp[i - 1][0] == 1)
+                dp[i][0] = 1;
+        }
+        for(int i = 1; i < m; i++) {
+            if(first.charAt(0) == second.charAt(i) || dp[0][i - 1] == 1)
+                dp[0][i] = 1;
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
+                boolean check = first.charAt(i) == second.charAt(j);
+                int value = dp[i - 1][j - 1];
+                if(check) value++;
+                dp[i][j] = Math.max(value, Math.max(dp[i - 1][j], dp[i][j - 1]));
             }
         }
-        
-        System.out.println(dp[c1.length][c2.length]);
+
+        System.out.println(dp[n - 1][m - 1]);
     }
 }
