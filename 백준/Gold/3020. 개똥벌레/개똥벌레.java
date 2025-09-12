9b21100fa40edd55749b3038708598b7b19f18d1
ChangHyun Yoo <jakey1110@naver.com>
2025-09-13 00:25:24 +0900

    [Gold V] Title: 개똥벌레, Time: 288 ms, Memory: 31452 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -1,79 +1,42 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static int[] under;
-    static int[] over;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int H = Integer.parseInt(st.nextToken());
 
-        under = new int[N / 2];
-        over = new int[N / 2];
+        int[] down = new int[H + 1];
+        int[] up = new int[H + 1];
 
         for(int i = 0; i < N; i++) {
-            if(i % 2 == 0) {
-                under[i / 2] = Integer.parseInt(br.readLine());
-            } else {
-                over[i / 2] = Integer.parseInt(br.readLine());
-            }
+            int value = Integer.parseInt(br.readLine());
+
+            if(i % 2 == 0) down[value]++;
+            else up[H - value + 1]++;
         }
 
-        Arrays.sort(under);
-        Arrays.sort(over);
+        for(int i = H - 1; i >= 0; i--) {
+            down[i] += down[i + 1];
+        }
+
+        for(int i = 1; i < H + 1; i++) {
+            up[i] += up[i - 1];
+        }
 
         int min = Integer.MAX_VALUE;
-        int num = 0;
-
-        for(int n = 1; n <= H; n++) {
-            int underLower = binarySearchUnder(n);
-            int overLower = binarySearchOver(H + 1 - n);
-
-            if(N - underLower - overLower < min) {
-                min = N - underLower - overLower;
-                num = 1;
-            } else if(N - underLower - overLower == min) {
-                num++;
-            }
+        int count = 0;
+        for(int i = 1; i < H + 1; i++) {
+            if(min > down[i] + up[i]) {
+                min = down[i] + up[i];
+                count = 1;
+            } else if(min == down[i] + up[i]) count++;
         }
 
-        System.out.println(min + " " + num);
-    }
-
-    private static int binarySearchOver(int n) {
-
-        int low = 0;
-        int high = over.length;
-        while(low < high) {
-            int mid = low + (high - low) / 2;
-            if(n <= over[mid]) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return low;
-    }
-
-    private static int binarySearchUnder(int n) {
-
-        int low = 0;
-        int high = under.length;
-        while(low < high) {
-            int mid = low + (high - low) / 2;
-            if(n <= under[mid]) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return low;
+        System.out.println(min + " " + count);
     }
 }
