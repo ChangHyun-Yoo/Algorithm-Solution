6acae9ff6160c155515aa6dabae19396544f2ccd
ChangHyun Yoo <jakey1110@naver.com>
2025-09-15 21:08:55 +0900

    [Gold V] Title: 최소비용 구하기, Time: 448 ms, Memory: 50856 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,75 +1,67 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static int[][] buses;
-    static boolean[] visited;
-    static int[] min;
-
-    static class Node implements Comparable<Node> {
-        int num;
-        int dis;
-
-        public Node(int num, int dis) {
-            this.num = num;
-            this.dis = dis;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.dis - o.dis;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
 
-        buses = new int[N + 1][N + 1];
-        visited = new boolean[N + 1];
-        min = new int[N + 1];
-
-        for(int[] bus : buses) {
-            Arrays.fill(bus, -1);
+        List<List<Node>> roads = new ArrayList<>();
+        for(int i = 0; i < N + 1; i++) {
+            roads.add(new ArrayList<>());
         }
-        Arrays.fill(min, Integer.MAX_VALUE);
 
         for(int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            if(buses[s][e] == -1 || buses[s][e] > m) {
-                buses[s][e] = m;
-            }
+            int d = Integer.parseInt(st.nextToken());
+
+            roads.get(s).add(new Node(e, d));
         }
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int s = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        int[] min = new int[N + 1];
+        Arrays.fill(min, Integer.MAX_VALUE);
 
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(s, 0));
-        min[s] = 0;
+        pq.offer(new Node(start, 0));
+
         while(!pq.isEmpty()) {
-            Node node = pq.poll();
+            Node now = pq.poll();
 
-            if(visited[node.num]) {
-                continue;
-            }
+            if(now.dis > min[now.x]) continue;
+            min[now.x] = now.dis;
 
-            visited[node.num] = true;
-
-            for(int i = 1; i < N + 1; i++) {
-                if(buses[node.num][i] != -1 && min[i] > min[node.num] + buses[node.num][i]) {
-                    min[i] = min[node.num] + buses[node.num][i];
-                    pq.offer(new Node(i, min[i]));
+            for(Node next: roads.get(now.x)) {
+                if(now.dis + next.dis < min[next.x]) {
+                    min[next.x] = now.dis + next.dis;
+                    pq.offer(new Node(next.x, min[next.x]));
                 }
             }
         }
+        
+        System.out.println(min[end]);
+    }
 
-        System.out.println(min[e]);
+    static class Node implements Comparable<Node> {
+        int x;
+        int dis;
+
+        public Node(int x, int dis) {
+            this.x = x;
+            this.dis = dis;
+        }
+
+        public int compareTo(Node n) {
+            return this.dis - n.dis;
+        }
     }
 }
