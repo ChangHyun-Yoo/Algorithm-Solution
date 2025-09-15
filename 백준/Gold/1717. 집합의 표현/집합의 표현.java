23184738b807202d14dc0903d5f7aefda373355d
ChangHyun Yoo <jakey1110@naver.com>
2025-09-15 21:24:18 +0900

    [Gold V] Title: 집합의 표현, Time: 612 ms, Memory: 60012 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,64 +1,60 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static int[] parent;
-    static int[] height;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        //초기화
-        parent = new int[n + 1];
+        int[] parent = new int[n + 1];
         for(int i = 0; i < parent.length; i++) {
             parent[i] = i;
         }
-        height = new int[n + 1];
-        Arrays.fill(height, 1);
+        int[] height = new int[n + 1];
 
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int k = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+
+            int c = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            if(k == 1) {
-                if(findRoot(a) == findRoot(b))
-                    sb.append("YES").append('\n');
-                else
-                    sb.append("NO").append('\n');
+
+            if(c == 0) {
+                union(a, b, parent, height);
             } else {
-                union(a, b);
+                if(findParent(a, parent) == findParent(b, parent)) sb.append("YES");
+                else sb.append("NO");
+                sb.append('\n');
             }
         }
-        System.out.println(sb);
+
+        System.out.print(sb.toString());
     }
 
-    static int findRoot(int i) {
-        if(i == parent[i])
-            return i;
+    static int findParent(int x, int[] parent) {
+        if(x == parent[x]) return x;
 
-        return findRoot(parent[i]);
+        return parent[x] = findParent(parent[x], parent);
     }
 
-    static void union(int a, int b) {
-        int rootA = findRoot(a);
-        int rootB = findRoot(b);
-        if(rootA == rootB)
-            return;
+    static void union(int a, int b, int[] parent, int[] height) {
+        int rootA = findParent(a, parent);
+        int rootB = findParent(b, parent);
+
+        if(rootA == rootB) return;
 
         if(height[rootA] > height[rootB]) {
             parent[rootB] = rootA;
-        } else if(height[rootB] > height[rootA]) {
+        } else if(height[rootA] < height[rootB]) {
             parent[rootA] = rootB;
         } else {
             parent[rootA] = rootB;
             height[rootB]++;
         }
     }
-
 }
