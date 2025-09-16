0a339ef31646e0b6ee2824b3e1bff89f504932c7
ChangHyun Yoo <jakey1110@naver.com>
2025-09-16 22:51:04 +0900

    [Gold V] Title: 트리와 쿼리, Time: 608 ms, Memory: 80012 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -1,56 +1,62 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static List<List<Integer>> roads = new ArrayList<>();
-    static boolean[] visited;
-    static int[] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
         int Q = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < N + 1; i++) {
+        List<List<Integer>> roads = new ArrayList<>();
+        for(int i = 0; i <= N; i++) {
             roads.add(new ArrayList<>());
         }
-        visited = new boolean[N + 1];
-        dp = new int[N + 1];
+
         for(int i = 0; i < N - 1; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            roads.get(a).add(b);
-            roads.get(b).add(a);
+            st = new StringTokenizer(br.readLine());
+
+            int U = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            roads.get(U).add(V);
+            roads.get(V).add(U);
         }
-        dp(R);
+
+        boolean[] visited = new boolean[N + 1];
+        int[] dp = new int[N + 1];
+
+        dfs(R, visited, dp, roads);
+
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < Q; i++) {
-            sb.append(dp[Integer.parseInt(br.readLine())]).append('\n');
+        for(int q = 0; q < Q; q++) {
+            int query = Integer.parseInt(br.readLine());
+
+            sb.append(dp[query]).append('\n');
         }
-        System.out.println(sb);
+        System.out.print(sb.toString());
     }
 
-    static int dp(int n) {
-        visited[n] = true;
+    static int dfs(int current, boolean[] visited, int[] dp, List<List<Integer>> roads) {
+        visited[current] = true;
 
-        int answer = 0;
-        for(int next: roads.get(n)) {
+        int num = 0;
+        for(int next: roads.get(current)) {
+            if(!visited[next]) num++;
+        }
+
+        if(num == 0) return dp[current] = 1;
+
+        int value = 0;
+        for(int next: roads.get(current)) {
             if(!visited[next]) {
-                answer += dp(next);
+                value += dfs(next, visited, dp, roads);
             }
         }
 
-        if(answer == 0) {
-            dp[n] = 1;
-        } else {
-            dp[n] = answer + 1;
-        }
-
-        return dp[n];
+        return dp[current] = value + 1;
     }
 }
-
