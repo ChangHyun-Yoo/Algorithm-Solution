c1bc119503322e32e889d9d90810b117dd5cce78
ChangHyun Yoo <jakey1110@naver.com>
2025-09-17 22:52:29 +0900

    [Gold V] Title: 입국심사, Time: 280 ms, Memory: 23536 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -5,36 +5,36 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        long[] value = new long[N];
-        for (int i = 0; i < N; i++) {
-            value[i] = Long.parseLong(br.readLine());
+        long[] times = new long[N];
+
+        for(int i = 0; i < N; i++) {
+            times[i] = Long.parseLong(br.readLine());
         }
 
-        long low = 1;
-        long high = (long) Math.pow(10, 18);
+        long low = 0;
+        long high = Long.MAX_VALUE;
 
         while(low < high) {
-            long result = 0;
             long mid = low + (high - low) / 2;
 
-            for (int i = 0; i < N; i++) {
-                result += mid / value[i];
-                if(result >= M) break;
+            long num = 0;
+            for(int i = 0; i < N; i++) {
+                num += mid / times[i];
+                if(num >= M) break;
             }
 
-            if(result >= M) {
-                high = mid;
-            } else {
+            if(num < M) {
                 low = mid + 1;
+            } else {
+                high = mid;
             }
         }
 
         System.out.println(low);
     }
-
 }
