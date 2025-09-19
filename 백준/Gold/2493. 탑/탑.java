02e2191542823b884105b6117d9aa9460cb23d36
ChangHyun Yoo <jakey1110@naver.com>
2025-09-19 23:37:27 +0900

    [Gold V] Title: 탑, Time: 768 ms, Memory: 90064 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,40 +5,43 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
         Stack<Node> stack = new Stack<>();
-        stack.push(new Node(Integer.parseInt(st.nextToken()), 1));
-        sb.append(0).append(' ');
-        for(int i = 2; i < N + 1; i++) {
-            int current = Integer.parseInt(st.nextToken());
-
-            while(!stack.isEmpty() && stack.peek().num <= current) {
-                stack.pop();
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= N; i++) {
+            int height = Integer.parseInt(st.nextToken());
 
             if(stack.isEmpty()) {
                 sb.append(0).append(' ');
+                stack.add(new Node(i, height));
+                continue;
+            }
+
+            // 자신보다 크거나 같은 탑을 만날 때까지 제거
+            while(!stack.isEmpty() && stack.peek().height < height) {
+                stack.pop();
+            }
+            if(stack.isEmpty()) {
+                sb.append(0).append(' ');
+                stack.add(new Node(i, height));
             } else {
                 sb.append(stack.peek().index).append(' ');
+                stack.add(new Node(i, height));
             }
-            stack.push(new Node(current, i));
         }
 
-        System.out.println(sb);
+        System.out.print(sb.toString());
     }
 
     static class Node {
-        int num;
         int index;
+        int height;
 
-        public Node(int num, int index) {
-            this.num = num;
+        public Node(int index, int height) {
             this.index = index;
+            this.height = height;
         }
     }
 }
