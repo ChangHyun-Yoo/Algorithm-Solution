b5142aeee2b4316badaf1ca75bccd05a2c993f85
ChangHyun Yoo <jakey1110@naver.com>
2025-09-19 22:57:53 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 320 ms, Memory: 50812 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,51 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static int[][] info;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        info = new int[N][N];
+        char[][] chs = new char[N][N];
 
-        dq(0, 0, N);
+        dq(0, N, 0, N, chs);
 
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                if(info[i][j] == 1) {
-                    sb.append(" ");
-                } else {
-                    sb.append("*");
-                }
+                if(chs[i][j] == '*') sb.append('*');
+                else sb.append(' ');
             }
             sb.append('\n');
         }
-        System.out.println(sb);
+
+        System.out.print(sb.toString());
     }
 
-    static void dq(int x, int y, int length) {
-        if(length == 1) {
+    static void dq(int sx, int ex, int sy, int ey, char[][] chs) {
+
+        if(ex - sx == 1) {
+            chs[sx][sy] = '*';
             return;
         }
 
-        for(int i = x + length / 3; i < x + length * 2 / 3; i++) {
-            for(int j = y + length / 3; j < y + length * 2 / 3; j++) {
-                info[i][j] = 1;
-            }
-        }
+        int d = (ex - sx) / 3;
 
-        dq(x, y, length / 3);
-        dq(x, y + length / 3, length / 3);
-        dq(x, y + 2  * length / 3, length / 3);
-        dq(x + length / 3, y, length / 3);
-        dq(x + length / 3, y + 2  * length / 3, length / 3);
-        dq(x + 2 * length / 3, y, length / 3);
-        dq(x + 2 * length / 3, y + length / 3, length / 3);
-        dq(x + 2 * length / 3, y + 2 * length / 3, length / 3);
+        dq(sx, sx + d, sy, sy + d, chs);
+        dq(sx, sx + d, sy + d, sy + 2 * d, chs);
+        dq(sx, sx + d, sy + 2 * d, ey, chs);
+
+        dq(sx + d, sx + 2 * d, sy, sy + d, chs);
+        dq(sx + d, sx + 2 * d, sy + 2 * d, ey, chs);
+
+        dq(sx + 2 * d, ex, sy, sy + d, chs);
+        dq(sx + 2 * d, ex, sy + d, sy + 2 * d, chs);
+        dq(sx + 2 * d, ex, sy + 2 * d, ey, chs);
     }
 }
