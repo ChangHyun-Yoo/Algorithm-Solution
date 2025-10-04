7d02686080d82ec086ff35565ec49f526d4a0812
ChangHyun Yoo <jakey1110@naver.com>
2025-10-04 23:18:48 +0900

    [Gold III] Title: 개미굴, Time: 144 ms, Memory: 15476 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.java
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -1,56 +1,67 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Trie trie = new Trie();
+        TrieNode root = trie.root;
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int K = Integer.parseInt(st.nextToken());
+
+            TrieNode current = root;
+            for(int k = 0; k < K; k++) {
+                String input = st.nextToken();
+
+                if(!current.next.containsKey(input)) {
+                    current.next.put(input, new TrieNode(input));
+                }
+
+                current = current.next.get(input);
+            }
+        }
+
+        for(String key: root.next.keySet()) {
+            dfs(root.next.get(key), 0, sb);
+        }
+
+        System.out.print(sb.toString());
+    }
+
+    static void dfs(TrieNode current, int floor, StringBuilder sb) {
+        for(int f = 0; f < floor; f++) {
+            sb.append("--");
+        }
+        sb.append(current.s).append('\n');
+
+        for(String key: current.next.keySet()) {
+            dfs(current.next.get(key), floor + 1, sb);
+        }
+    }
 
     static class TrieNode {
-        Map<String, TrieNode> child = new TreeMap<>();
-        int d;
+        String s;
+        TreeMap<String, TrieNode> next;
+
+        public TrieNode(String s) {
+            this.s = s;
+            next = new TreeMap<>();
+        }
     }
 
     static class Trie {
-        static TrieNode root;
+        TrieNode root;
 
         public Trie() {
-            root = new TrieNode();
-        }
-
-        static void insert(String[] strs) {
-            TrieNode node = root;
-            for(String s: strs) {
-                node = node.child.computeIfAbsent(s, key -> new TrieNode());
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Trie trie = new Trie();
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int n = Integer.parseInt(st.nextToken());
-            String[] strs = new String[n];
-            for(int j = 0; j < n; j++) {
-                strs[j] = st.nextToken();
-            }
-            trie.insert(strs);
-        }
-        sb = new StringBuilder();
-        dfs(trie.root, 0);
-        System.out.println(sb);
-    }
-
-    static void dfs(TrieNode trieNode, int d) {
-        for(String key: trieNode.child.keySet()) {
-            for(int i = 0; i < d; i++) {
-                sb.append("-");
-            }
-            sb.append(key).append('\n');
-            dfs(trieNode.child.get(key), d + 2);
+            this.root = new TrieNode("root");
         }
     }
 }
-
