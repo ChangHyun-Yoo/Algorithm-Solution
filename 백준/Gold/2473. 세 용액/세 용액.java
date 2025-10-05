678b0052c28f024db2a251d2f33787faf7b1a1fc
ChangHyun Yoo <jakey1110@naver.com>
2025-10-05 18:32:40 +0900

    [Gold III] Title: 세 용액, Time: 208 ms, Memory: 17300 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -1,100 +1,49 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static long[] nums;
-    static long[] answer = new long[3];
     static long min = Long.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        nums = new long[N];
+        long[] nums = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             nums[i] = Long.parseLong(st.nextToken());
         }
 
         Arrays.sort(nums);
-        for(int i = 0; i < N - 2; i++) {
-            for(int j = i + 1; j < N - 1; j++) {
-                long sum = nums[i] + nums[j];
-                int index = lowerBound(j + 1, N, -sum);
-                if(index == j + 1) {
-                    if(min > Math.abs(sum + nums[j + 1])) {
-                        min = Math.abs(sum + nums[j + 1]);
-                        answer[0] = nums[i];
-                        answer[1] = nums[j];
-                        answer[2] = nums[j + 1];
-                    }
-                } else if(index == N) {
-                    if(min > Math.abs(sum + nums[index - 1])) {
-                        min = Math.abs(sum + nums[index - 1]);
-                        answer[0] = nums[i];
-                        answer[1] = nums[j];
-                        answer[2] = nums[index - 1];
-                    }
-                } else if(index == N - 1) {
-                    if(Math.abs(sum + nums[N - 1]) > Math.abs(sum + nums[N - 2])) {
-                        if(min > Math.abs(sum + nums[N - 2])) {
-                            min = Math.abs(sum + nums[N - 2]);
-                            answer[0] = nums[i];
-                            answer[1] = nums[j];
-                            answer[2] = nums[N - 2];
-                        }
-                    } else {
-                        if(min > Math.abs(sum + nums[N - 1])) {
-                            min = Math.abs(sum + nums[N - 1]);
-                            answer[0] = nums[i];
-                            answer[1] = nums[j];
-                            answer[2] = nums[N - 1];
-                        }
-                    }
-                } else {
-                    if(nums[index] == -sum) {
-                        System.out.println(nums[i] + " " + nums[j] + " " + nums[index]);
+
+        long answer1 = 0;
+        long answer2 = 0;
+        long answer3 = 0;
+
+        for(int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left < right) {
+                long sum = nums[i] + nums[left] + nums[right];
+
+                if(min > Math.abs(sum)) {
+                    answer1 = nums[i];
+                    answer2 = nums[left];
+                    answer3 = nums[right];
+
+                    min = Math.abs(sum);
+                    if(min == 0) {
+                        System.out.println(answer1 + " " + answer2 + " " + answer3);
                         return;
-                    } else {
-                        if(Math.abs(sum + nums[index]) > Math.abs(sum + nums[index - 1])) {
-                            if(min > Math.abs(sum + nums[index - 1])) {
-                                min = Math.abs(sum + nums[index - 1]);
-                                answer[0] = nums[i];
-                                answer[1] = nums[j];
-                                answer[2] = nums[index - 1];
-                            }
-                        } else {
-                            if(min > Math.abs(sum + nums[index])) {
-                                min = Math.abs(sum + nums[index]);
-                                answer[0] = nums[i];
-                                answer[1] = nums[j];
-                                answer[2] = nums[index];
-                            }
-                        }
                     }
                 }
+
+                if(sum > 0) right--;
+                else left++;
             }
         }
-        for(long a: answer) {
-            System.out.print(a + " ");
-        }
+        System.out.println(answer1 + " " + answer2 + " " + answer3);
     }
-
-    private static int lowerBound(int start, int end, long value) {
-        int low = start;
-        int high = end;
-
-        while(low < high) {
-            int mid = low + (high - low) / 2;
-
-            if(value <= nums[mid]) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return low;
-    }
-
 }
