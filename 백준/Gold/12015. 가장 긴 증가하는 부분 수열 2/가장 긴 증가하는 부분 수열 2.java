cd684fbe280bb7d11be5337c5a19f02b2e69e5b0
ChangHyun Yoo <jakey1110@naver.com>
2025-10-06 13:54:12 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 492 ms, Memory: 92212 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,56 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static int[] info;
-    static int[] result;
-    static int length;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        info = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] list = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
-            info[i] = Integer.parseInt(st.nextToken());
+            list[i] = Integer.parseInt(st.nextToken());
         }
 
-        result = new int[N];
-        result[0] = info[0];
-        length = 1;
-        int current = 1;
-        while(current != N) {
-            int index = binarySearch(info[current]);
-            if(index == -1) {
-                result[length++] = info[current++];
+        int length = 1;
+
+        for(int i = 1; i < N; i++) {
+            int current = list[i];
+
+            int low = 0;
+            int high = length;
+            while(low < high) {
+                int mid = low + (high - low) / 2;
+
+                if(list[mid] >= current) {
+                    high = mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            if(low == length) {
+                list[low] = current;
+                length++;
             } else {
-                result[index] = info[current++];
+                if(list[low] > current) {
+                    list[low] = current;
+                }
             }
         }
 
         System.out.println(length);
-    }
-
-    static int binarySearch(int value) {
-
-        int low = 0;
-        int high = length;
-
-        while(low < high) {
-            int mid = low + (high - low) / 2;
-            if(value <= result[mid]) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        if(low == length) {
-            return -1;
-        } else {
-            return low;
-        }
     }
 }
