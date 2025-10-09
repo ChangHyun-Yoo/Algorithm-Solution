93f82b8d7896118fdb88f0e0ef240697f614abef
ChangHyun Yoo <jakey1110@naver.com>
2025-10-09 19:45:14 +0900

    [Gold III] Title: ACM Craft, Time: 780 ms, Memory: 250264 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,80 +1,73 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static int[] time;
-    static int[] before;
-    static List<List<Integer>> next;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
         for(int t = 0; t < T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
-            next = new ArrayList<>();
+
+            int[] times = new int[N + 1];
+            int[] before = new int[N + 1];
+            List<List<Integer>> roads = new ArrayList<>();
             for(int i = 0; i < N + 1; i++) {
-                next.add(new ArrayList<>());
+                roads.add(new ArrayList<>());
             }
-
-            time = new int[N + 1];
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             for(int i = 1; i < N + 1; i++) {
-                time[i] = Integer.parseInt(st.nextToken());
+                times[i] = Integer.parseInt(st.nextToken());
             }
 
-            before = new int[N + 1];
-
-            for(int i = 0; i < K; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                int s = Integer.parseInt(st.nextToken());
-                int e = Integer.parseInt(st.nextToken());
-                next.get(s).add(e);
-                before[e]++;
+            for(int j = 0; j < K; j++) {
+                st = new StringTokenizer(br.readLine());
+                int X = Integer.parseInt(st.nextToken());
+                int Y = Integer.parseInt(st.nextToken());
+                roads.get(X).add(Y);
+                before[Y]++;
             }
-
             int W = Integer.parseInt(br.readLine());
-            int[] min = new int[N + 1];
 
             PriorityQueue<Node> pq = new PriorityQueue<>();
             for(int i = 1; i < N + 1; i++) {
-                if(before[i] == 0) {
-                    pq.offer(new Node(i, time[i]));
-                }
+                if(before[i] == 0) pq.offer(new Node(i, times[i]));
             }
 
+            int[] answer = new int[N + 1];
             while(!pq.isEmpty()) {
                 Node now = pq.poll();
-                if(now.num == W) {
-                    min[now.num] = now.time;
-                    break;
-                }
-                for(int ne: next.get(now.num)) {
-                    before[ne]--;
-                    if(before[ne] == 0) {
-                        pq.offer(new Node(ne, time[ne] + now.time));
+
+                answer[now.x] = Math.max(answer[now.x], now.time);
+
+                for(int next: roads.get(now.x)) {
+                    if(--before[next] == 0) {
+                        pq.offer(new Node(next, answer[now.x] + times[next]));
                     }
                 }
             }
-            System.out.println(min[W]);
+            sb.append(answer[W]).append('\n');
         }
+
+        System.out.print(sb.toString());
     }
 
     static class Node implements Comparable<Node> {
-        int num;
+        int x;
         int time;
 
-        public Node(int num, int time) {
-            this.num = num;
+        public Node(int x, int time) {
+            this.x = x;
             this.time = time;
         }
 
-        public int compareTo(Node o) {
-            return this.time - o.time;
+        public int compareTo(Node n) {
+            return this.time - n.time;
         }
     }
 }
