c55a13178f427a025173db7fffa4f7332fce7694
ChangHyun Yoo <jakey1110@naver.com>
2025-10-09 20:38:19 +0900

    [Gold III] Title: 줄 세우기, Time: 492 ms, Memory: 47496 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,49 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static int[] beforeNum;
-    static List<List<Integer>> order = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        beforeNum = new int[N + 1];
-        for(int i = 0; i < N + 1; i++) {
-            order.add(new ArrayList<>());
-        }
-        for(int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int b = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            beforeNum[a]++;
-            order.get(b).add(a);
-        }
 
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 1; i < N + 1; i++) {
-            if(beforeNum[i] == 0) {
-                q.offer(i);
-            }
+        List<List<Integer>> roads = new ArrayList<>();
+        for(int i = 0; i < N + 1; i++) {
+            roads.add(new ArrayList<>());
+        }
+        int[] before = new int[N + 1];
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            before[B]++;
+            roads.get(A).add(B);
         }
 
         StringBuilder sb = new StringBuilder();
-        while(!q.isEmpty()) {
-            int i = q.poll();
-            sb.append(i + " ");
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 1; i < N + 1; i++) {
+            if(before[i] == 0) q.offer(i);
+        }
 
-            for(int next: order.get(i)) {
-                beforeNum[next]--;
-                if(beforeNum[next] == 0) {
-                    q.offer(next);
-                }
+        while(!q.isEmpty()) {
+            int now = q.poll();
+
+            sb.append(now).append(' ');
+
+            for(int next: roads.get(now)) {
+                if(--before[next] == 0) q.offer(next);
             }
         }
 
-        System.out.println(sb);
+        System.out.print(sb.toString());
     }
-
 }
