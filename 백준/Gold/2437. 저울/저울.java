04c56fd7c258d8b349aa528ef56e72b9e2d465ac
ChangHyun Yoo <jakey1110@naver.com>
2025-10-14 14:45:49 +0900

    [Gold II] Title: 저울, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/2437. 저울/저울.java
+++ 백준/Gold/2437. 저울/저울.java
@@ -5,44 +5,35 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] weights = new int[N];
+
         for(int i = 0; i < N; i++) {
-            pq.offer(Long.parseLong(st.nextToken()));
+            weights[i] = Integer.parseInt(st.nextToken());
         }
 
-        if(pq.peek() != 1) {
+        Arrays.sort(weights);
+
+        if(weights[0] != 1) {
             System.out.println(1);
             return;
         }
 
-        long min = pq.poll();//1
-        long max = min;//1
-        long answer = 0;
-        while(!pq.isEmpty()) {
-            long polled = pq.poll();
-            if(polled == max + 1) {
-                if(min + polled > max + 2) {
-                    answer = max + 2;
-                    break;
-                } else {
-                    max += polled;
-                }
-            } else {
-                if(max - min >= polled - 1) {
-                    max += polled;
-                } else {//측정 불가능한 게 나타날 때
-                    answer = max + 1;
-                    break;
-                }
-            }
+        int answer = -1;
+        boolean breaked = false;
+        int max = 1;
+        for(int i = 1; i < weights.length; i++) {
+            if(weights[i] > max + 1) {
+                answer = max + 1;
+                breaked = true;
+                break;
+            } else max += weights[i];
         }
-        if(answer == 0) {
-            System.out.println(max + 1);
-        } else {
-            System.out.println(answer);
-        }
+
+        if(!breaked) answer = max + 1;
+
+        System.out.println(answer);
     }
 }
