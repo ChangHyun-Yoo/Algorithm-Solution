b8d5def1fc68f1163f36d277901a845a115fc6fe
ChangHyun Yoo <jakey1110@naver.com>
2025-10-15 22:19:37 +0900

    [Gold II] Title: 공항, Time: 228 ms, Memory: 22988 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.java
+++ 백준/Gold/10775. 공항/공항.java
@@ -1,9 +1,8 @@
-import java.io.*;
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static int[] parent;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,35 +10,45 @@ public class Main {
         int G = Integer.parseInt(br.readLine());
         int P = Integer.parseInt(br.readLine());
 
-        parent = new int[G + 1];
-        for(int i = 0; i < G + 1; i++) {
+        int[] parent = new int[G + 1];
+        for(int i = 0; i < parent.length; i++) {
             parent[i] = i;
         }
 
-        int answer = 0;
-        for(int i = 0; i < P; i++) {
-            int p = Integer.parseInt(br.readLine());
-            int find = findRoot(p);
-            if(find == 0) break;
-            else {
-                union(find - 1, find);
-                answer++;
-            }
+        int[] airplanes = new int[P];
+        for(int p = 0; p < P; p++) {
+            airplanes[p] = Integer.parseInt(br.readLine());
         }
+
+        int answer = 0;
+        for(int i = 0; i < airplanes.length; i++) {
+            int airplane = airplanes[i];
+
+            int p = findParent(airplane, parent);
+
+            if(p == 0) break;
+            else answer++;
+
+            union(p - 1, p, parent);
+        }
+
         System.out.println(answer);
     }
 
-    static int findRoot(int i) {
-        if(i == parent[i]) return i;
+    static int findParent(int airplane, int[] parent) {
+        if(airplane == parent[airplane]) return airplane;
 
-        return parent[i] = findRoot(parent[i]);
+        return parent[airplane] = findParent(parent[airplane], parent);
     }
 
-    static void union(int i, int j) {
-        int rootI = findRoot(i);
-        int rootJ = findRoot(j);
+    static void union(int i, int j, int[] parent) {
+        int rootI = findParent(i, parent);
+        int rootJ = findParent(j, parent);
 
-        if(rootI != rootJ)
+        if(rootI < rootJ) {
             parent[rootJ] = rootI;
+        } else if(rootI > rootJ) {
+            parent[rootI] = rootJ;
+        }
     }
 }
