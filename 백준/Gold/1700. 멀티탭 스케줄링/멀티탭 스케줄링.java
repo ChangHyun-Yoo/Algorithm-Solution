cc45f26bcb3dcde69b6a7e054799fc77212fe0db
ChangHyun Yoo <jakey1110@naver.com>
2025-10-15 13:34:42 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -5,55 +5,65 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        String[] lst = br.readLine().split(" ");
-
-        int index = 0;
-        List<Integer> mul = new ArrayList<>();
-        while(mul.size() != N && index < K) {
-            int i = Integer.parseInt(lst[index]);
-            if(!mul.contains(i)) {
-                mul.add(i);
-            }
-            index++;
+        int[] devices = new int[K];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < K; i++) {
+            int device = Integer.parseInt(st.nextToken());
+            devices[i] = device;
         }
 
+        Set<Integer> current = new HashSet<>();
+
         int answer = 0;
-        //적게 남은 것 부터 뽑기
-        for(int j = index; j < K; j++) {
-            int num = Integer.parseInt(lst[j]);//기기 번호
-            if(mul.contains(num)) {//이미 멀티탭이 꽂혀있으면
+        for(int i = 0; i < devices.length; i++) {
+            int device = devices[i];
+
+            if(current.size() < N || current.contains(device)) {
+                current.add(device);
                 continue;
-            } else {
-                List<Integer> recent = new ArrayList<>();
-                int remove = 0;
-                for(int m: mul) {
-                    boolean b = false;
-                    for(int k = j + 1; k < K; k++) {
-                        if(Integer.parseInt(lst[k]) == m) {//가까이 있는게 있을 경우
-                            recent.add(k);
-                            b = true;
-                            break;
-                        }
-                    }
-                    if(!b) {//이후로 나오지 않는 친구가 있으면
-                        remove = m;
+            }
+
+            Map<Integer, Integer> map = new HashMap<>();
+            int zero = -1;
+            for(int c: current) {
+                boolean exists = false;
+                for(int j = i + 1; j < K; j++) {
+                    // 이후에도 존재하는 경우
+                    if(devices[j] == c) {
+                        map.put(c, j);
+                        exists = true;
                         break;
                     }
                 }
-                //모두 다 근처에 있는 경우 가장 멀리 있는 제품을 뽑아
-                if(remove == 0) {
-                    remove = mul.get(recent.indexOf(Collections.max(recent)));
-                }
 
-                mul.remove(Integer.valueOf(remove));
-                mul.add(num);
-                answer++;
+                if(!exists) {
+                    zero = c;
+                    break;
+                }
             }
+
+            int max = -1;
+            int maxCurrent = -1;
+            if(zero == -1) {
+                for(int key: map.keySet()) {
+                    if(max < map.get(key)) {
+                        max = map.get(key);
+                        maxCurrent = key;
+                    }
+                }
+                zero = maxCurrent;
+            }
+
+            current.remove(zero);
+            answer++;
+            current.add(device);
         }
+
         System.out.println(answer);
     }
 }
