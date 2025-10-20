94bf3b8bedb0b08a5be03cc31bd8b03ae6cdecdc
ChangHyun Yoo <jakey1110@naver.com>
2025-10-21 00:04:05 +0900

    [Gold III] Title: 중량제한, Time: 460 ms, Memory: 55520 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,82 +1,69 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static boolean[] visited;
-    static List<List<Node>> roads;
-    static int start;
-    static int end;
-    static int N;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        roads = new ArrayList<>();
+
+        List<List<Node>> roads = new ArrayList<>();
         for(int i = 0; i < N + 1; i++) {
             roads.add(new ArrayList<>());
         }
-        for(int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+
+        for(int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
             int C = Integer.parseInt(st.nextToken());
+
             roads.get(A).add(new Node(B, C));
             roads.get(B).add(new Node(A, C));
         }
 
-        st = new StringTokenizer(br.readLine(), " ");
-        start = Integer.parseInt(st.nextToken());
-        end = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
 
-        int low = 1;
-        int high = 1000000001;
-        while(low < high) {
-            int mid = low + (high - low) / 2;
+        int[] max = new int[N + 1];
 
-            if(bfs(mid)) {//건널 수 있으면
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
-        }
-
-        System.out.println(low - 1);
-    }
-
-    private static boolean bfs(int mid) {
-        boolean result = false;
-        Queue<Node> q = new LinkedList<>();
-        visited = new boolean[N + 1];
-        q.offer(new Node(start, 0));
-        visited[start] = true;
-
-        while(!q.isEmpty()) {
-            Node now = q.poll();
-            if(now.num == end) {
-                result = true;
-                break;
-            }
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(s, Integer.MAX_VALUE));
+        while(!pq.isEmpty()) {
+            Node now = pq.poll();
+            
+            if(now.dis < max[now.num]) continue;
+            max[now.num] = now.dis;
 
             for(Node next: roads.get(now.num)) {
-                if(!visited[next.num] && next.weight >= mid) {
-                    q.offer(new Node(next.num, next.weight));
-                    visited[next.num] = true;
+                int min = Math.min(now.dis, next.dis);
+
+                if(max[next.num] < min) {
+                    max[next.num] = min;
+                    pq.offer(new Node(next.num, min));
                 }
             }
         }
-        return result;
+
+        System.out.println(max[e]);
     }
 
-    private static class Node {
+    static class Node implements Comparable<Node> {
         int num;
-        int weight;
+        int dis;
 
-        public Node(int num, int weight) {
+        public Node(int num, int dis) {
             this.num = num;
-            this.weight = weight;
+            this.dis = dis;
+        }
+
+        public int compareTo(Node n) {
+            return n.dis - this.dis;
         }
     }
 }
