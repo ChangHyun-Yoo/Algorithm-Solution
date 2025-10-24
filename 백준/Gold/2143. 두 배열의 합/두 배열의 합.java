8b208eab28c4df2c4fc6067a076bbd7e9dcaf011
ChangHyun Yoo <jakey1110@naver.com>
2025-10-24 23:45:48 +0900

    [Gold III] Title: 두 배열의 합, Time: 496 ms, Memory: 116760 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -1,56 +1,56 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long T = Long.parseLong(br.readLine());
-        long n = Long.parseLong(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        List<Integer> A = new ArrayList<>();
-        A.add(0);
-        int i = 0;
-        while(st.hasMoreTokens()) {
-            A.add(Integer.parseInt(st.nextToken()) + A.get(i++));
+
+        int T = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long[] sumA = new long[n + 1];
+        for(int i = 1; i < n + 1; i++) {
+            sumA[i] = Long.parseLong(st.nextToken()) + sumA[i - 1];
         }
-        long m = Long.parseLong(br.readLine());
-        st = new StringTokenizer(br.readLine(), " ");
-        List<Integer> B = new ArrayList<>();
-        B.add(0);
-        i = 0;
-        while(st.hasMoreTokens()) {
-            B.add(Integer.parseInt(st.nextToken()) + B.get(i++));
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        long[] sumB = new long[m + 1];
+        for(int i = 1; i < m + 1; i++) {
+            sumB[i] = Long.parseLong(st.nextToken()) + sumB[i - 1];
         }
-        Map<Long, Long> tableA = new HashMap<>();
-        for(int j = 0; j < n; j++) {
-            for(int k = j + 1; k <= n; k++) {
-                long test = A.get(k) - A.get(j);
-                if(tableA.get(test) == null) {
-                    tableA.put(test, (long) 1);
-                } else {
-                    tableA.replace(test, tableA.get(test) + 1);
-                }
+
+        Map<Long, Long> mapA = new HashMap<>();
+        Map<Long, Long> mapB = new HashMap<>();
+
+        for(int i = 0; i < sumA.length - 1; i++) {
+            for(int j = i + 1; j < sumA.length; j++) {
+                long value = sumA[j] - sumA[i];
+
+                if(!mapA.containsKey(value)) mapA.put(value, 1L);
+                else mapA.replace(value, mapA.get(value) + 1L);
             }
         }
-        Map<Long, Long> tableB = new HashMap<>();
-        for(int j = 0; j < m; j++) {
-            for(int k = j + 1; k <= m; k++) {
-                long test = B.get(k) - B.get(j);
-                if(tableB.get(test) == null) {
-                    tableB.put(test, (long) 1);
-                } else {
-                    tableB.replace(test, tableB.get(test) + 1);
-                }
+
+        for(int i = 0; i < sumB.length - 1; i++) {
+            for(int j = i + 1; j < sumB.length; j++) {
+                long value = sumB[j] - sumB[i];
+
+                if(!mapB.containsKey(value)) mapB.put(value, 1L);
+                else mapB.replace(value, mapB.get(value) + 1L);
             }
         }
-        Set<Long> setA = tableA.keySet();
+
         long answer = 0;
-        for(long s: setA) {
-            if(tableB.get(T - s) != null) {
-                answer += tableA.get(s) * tableB.get(T - s);
-            }
+        for(long key: mapA.keySet()) {
+            long need = T - key;
+
+            if(mapB.containsKey(need)) answer += mapA.get(key) * mapB.get(need);
         }
+
         System.out.println(answer);
     }
-
 }
